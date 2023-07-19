3c6110b9c6dc444fa0ca4c89caa6a012cb6b3ff9
Muhammad Ibrahim <IbDaGib@screencrib.com>
2023-07-19 14:06:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,32 +46,32 @@ import streamlit as st
 from streamlit_folium import folium_static
 from shapely.geometry import Point
 
-# List of tuples containing movie script data
-movie_scripts_data = [('USA', 100), ('France', 80)]  # Replace with your actual data
+# # List of tuples containing movie script data
+# movie_scripts_data = [('USA', 100), ('France', 80)]  # Replace with your actual data
 
-# Convert the list of tuples to a DataFrame
-df_movie_scripts = pd.DataFrame(movie_scripts_data, columns=['Country', 'Count'])
+# # Convert the list of tuples to a DataFrame
+# df_movie_scripts = pd.DataFrame(movie_scripts_data, columns=['Country', 'Count'])
 
-# Load the tax credit data
-df_tax_credits = pd.read_csv('tax_credits.csv')  # 'Country', 'TaxCreditsRate', 'Eligibility', 'Description', 'Link'
+# # Load the tax credit data
+# df_tax_credits = pd.read_csv('tax_credits.csv')  # 'Country', 'TaxCreditsRate', 'Eligibility', 'Description', 'Link'
 
-# Merge the dataframes
-df = pd.merge(df_movie_scripts, df_tax_credits, on='Country')
+# # Merge the dataframes
+# df = pd.merge(df_movie_scripts, df_tax_credits, on='Country')
 
-# Load the world map
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+# # Load the world map
+# world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
-# Merge with world map
-world['Country'] = world['name']
-df_world = pd.merge(world, df, on='Country', how='left')
-df_world['Count'] = df_world['Count'].fillna(0)  # Replace NaN counts with 0
+# # Merge with world map
+# world['Country'] = world['name']
+# df_world = pd.merge(world, df, on='Country', how='left')
+# df_world['Count'] = df_world['Count'].fillna(0)  # Replace NaN counts with 0
 
-# Generate the basic world map using geoplot
-plt.figure(figsize=(15, 10))
-gplt.choropleth(df_world, hue='Count', cmap='Greys', figsize=(10, 5))
+# # Generate the basic world map using geoplot
+# plt.figure(figsize=(15, 10))
+# gplt.choropleth(df_world, hue='Count', cmap='Greys', figsize=(10, 5))
 
-# Generate the interactive world map with folium
-map = folium.Map(location=[0, 0], zoom_start=2)
+# # Generate the interactive world map with folium
+# map = folium.Map(location=[0, 0], zoom_start=2)
 
 # Add the countries to the map with pop-up info
 # for i in range(len(df_world)):
@@ -92,4 +92,4 @@ map = folium.Map(location=[0, 0], zoom_start=2)
 
 # Use Streamlit to show the interactive map
 st.title('World Map with Movie Scripts and Tax Credits Data')
-folium_static(map)
+# folium_static(map)
