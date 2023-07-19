646dc41849ee12b080332ac6492213abf58adf12
Muhammad Ibrahim <IbDaGib@screencrib.com>
2023-07-19 14:25:14 -0400

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,59 +37,27 @@ In the meantime, below is an example of what you can do with just a few lines of
 #         .mark_circle(color='#0068c9', opacity=0.5)
 #         .encode(x='x:Q', y='y:Q'))
 
-import pandas as pd
-import geopandas as gpd
-# import matplotlib.pyplot as plt
-# import geoplot as gplt
-# import folium
+import pandas as pd 
+import plotly.express as px
 import streamlit as st
-# from streamlit_folium import folium_static
-# from shapely.geometry import Point
 
-# # List of tuples containing movie script data
-# movie_scripts_data = [('USA', 100), ('France', 80)]  # Replace with your actual data
+# Example list of tuples
+mentions = [('United States', 100), ('Canada', 50), ('Mexico', 20), 
+            ('United Kingdom', 10), ('France', 5), ('Germany', 3)]
 
-# # Convert the list of tuples to a DataFrame
-# df_movie_scripts = pd.DataFrame(movie_scripts_data, columns=['Country', 'Count'])
+# Convert to DataFrame
+mentions_df = pd.DataFrame(mentions, columns=['country', 'mentions'])
 
-# # Load the tax credit data
-# df_tax_credits = pd.read_csv('tax_credits.csv')  # 'Country', 'TaxCreditsRate', 'Eligibility', 'Description', 'Link'
+# Rest of code remains mostly the same...
+fig = px.choropleth(mentions_df, locations='country', 
+                    color='mentions', 
+                    hover_name='country',
+                    color_continuous_scale=px.colors.sequential.Greys)
+                    
+def get_popup_text(country):
+   # lookup tax data
+   ...
+   
+fig.update_traces(hovertemplate=get_popup_text) 
 
-# # Merge the dataframes
-# df = pd.merge(df_movie_scripts, df_tax_credits, on='Country')
-
-# # Load the world map
-# world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-
-# # Merge with world map
-# world['Country'] = world['name']
-# df_world = pd.merge(world, df, on='Country', how='left')
-# df_world['Count'] = df_world['Count'].fillna(0)  # Replace NaN counts with 0
-
-# # Generate the basic world map using geoplot
-# plt.figure(figsize=(15, 10))
-# gplt.choropleth(df_world, hue='Count', cmap='Greys', figsize=(10, 5))
-
-# # Generate the interactive world map with folium
-# map = folium.Map(location=[0, 0], zoom_start=2)
-
-# Add the countries to the map with pop-up info
-# for i in range(len(df_world)):
-#     try:
-#         location = [df_world.at[i, 'geometry'].centroid.y, df_world.at[i, 'geometry'].centroid.x]
-#         popup = folium.Popup(
-#             f"Country: {df_world.at[i, 'Country']}<br>"
-#             f"Count: {df_world.at[i, 'Count']}<br>"
-#             f"Tax credit rate: {df_world.at[i, 'TaxCreditsRate']}<br>"
-#             f"Eligibility: {df_world.at[i, 'Eligibility']}<br>"
-#             f"Description: {df_world.at[i, 'Description']}<br>"
-#             f"<a href='{df_world.at[i, 'Link']}'>Link</a>",
-#             max_width=300
-#         )
-#         folium.Marker(location, popup=popup).add_to(map)
-#     except:
-#         pass
-
-# Use Streamlit to show the interactive map
-st.title('World Map with Film Tax Credits Data')
-# folium_static(map)
+st.plotly_chart(fig)
